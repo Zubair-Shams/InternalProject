82e2634ddc540546e2179f9600e341c87e891334
Saeed ul Haq <saeedulhaquet@gmail.com>
2025-09-22 00:23:44 +0500

    Create appRoutes.js

---
+++ src/routes/appRoutes.js
@@ -0,0 +1,20 @@
+import Offer from "pages/offer";
+import { lazy } from "react";
+
+// Lazy imports
+const Home = lazy(() => import("pages/Home"));
+const About = lazy(() => import("pages/About"));
+const NotFound = lazy(() => import("pages/NotFound"));
+const Register = lazy(() => import("pages/register"));
+const ThankYou = lazy(() => import("pages/thankyou"));
+
+const appRoutes = [
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/register", element: <Register /> },
+  { path: "/thankyou", element: <ThankYou /> },
+  { path: "/offer", element: <Offer /> },
+  { path: "*", element: <NotFound /> },
+];
+
+export default appRoutes;
