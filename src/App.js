5e933df4c272efb73fad2c5bd5e6961fbac84f5b
Saeed ul Haq <saeedulhaquet@gmail.com>
2025-09-22 00:23:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "pages/Home";
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
 import GameLayout from "components/layout";
-import Register from "pages/register";
-import ThankYou from "pages/thankyou";
-
-const routes = [
-  { path: "/", element: <Home /> },
-  { path: "/about", element: <About /> },
-  { path: "/register", element: <Register /> },
-  { path: "/thankyou", element: <ThankYou /> },
-  { path: "*", element: <NotFound /> },
-];
+import appRoutes from "./routes/appRoutes";
+import { Suspense } from "react";
 
 function App() {
   return (
     <div className="App">
       <GameLayout>
-        <Routes>
-          {routes.map((route, index) => (
-            <Route key={index} path={route.path} element={route.element} />
-          ))}
-        </Routes>
+        <Suspense fallback={"loading ...."}>
+          <Routes>
+            {appRoutes.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </Suspense>
       </GameLayout>
     </div>
   );
